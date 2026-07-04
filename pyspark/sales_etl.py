236b707eabb7f4ef01ecfefe471718e2c419b618
sreerangakuntumalla21-dot <sreerangakuntumalla21@gmail.com>
2026-07-05 00:25:09 +0530

    Update sales_etl.py

--- pyspark/sales_etl.py
+++ pyspark/sales_etl.py
@@ -1,1 +1,17 @@
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder \
+    .appName("RetailMart ETL") \
+    .getOrCreate()
+
+customers_df = spark.read.csv(
+    "../datasets/customers.csv",
+    header=True,
+    inferSchema=True
+)
+
+selected_df = customers_df.select("Customer Name", "City")
+
+selected_df.show()
+
+spark.stop()
