a746f81ddbee6f0b40ec83feade7d1dbe95bdc32
sreerangakuntumalla21-dot <sreerangakuntumalla21@gmail.com>
2026-07-04 15:59:00 +0530

    Update inventory_generator.py

--- data_generation/inventory_generator.py
+++ data_generation/inventory_generator.py
@@ -1,1 +1,29 @@
 
+import pandas as pd
+import random
+
+inventory = []
+
+warehouses = [
+    "Hyderabad",
+    "Bengaluru",
+    "Chennai",
+    "Mumbai",
+    "Delhi"
+]
+
+for product_id in range(101, 111):
+
+    inventory.append({
+        "product_id": product_id,
+        "warehouse": random.choice(warehouses),
+        "available_stock": random.randint(10, 500)
+    })
+
+df = pd.DataFrame(inventory)
+
+print(df)
+
+df.to_csv("../datasets/inventory.csv", index=False)
+
+print("\nInventory CSV created successfully!")
