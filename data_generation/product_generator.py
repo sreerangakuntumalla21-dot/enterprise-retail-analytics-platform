85412040a8a15ed6756410791f68846d00d46e10
sreerangakuntumalla21-dot <sreerangakuntumalla21@gmail.com>
2026-07-04 15:20:17 +0530

    Update product_generator.py

--- data_generation/product_generator.py
+++ data_generation/product_generator.py
@@ -1,1 +1,32 @@
+import pandas as pd
+from faker import Faker
+import random
 
+fake = Faker("en_IN")
+
+products = []
+
+categories = ["Mobile", "Laptop","Headphones","Watch","Camera"]
+brands = ["Apple","Samsung","Sony","Dell","HP"]
+
+for product_id in range(101,111):
+
+    category = random.choice(categories)
+    brand = random.choice(brands)
+
+    product = {
+        "product_id": product_id,
+        "product_name": fake.word().capitalize(),
+        "category": category,
+        "brand": brand,
+        "price": random.randint(1000, 100000)
+    }
+
+    products.append(product)
+
+df = pd.DataFrame(products)
+
+print(df)
+
+df.to_csv("../datasets/products.csv", index=False)
+print("Products dataset generated and saved to products.csv")
